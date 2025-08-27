815859bdc7c814651b559cf61bb703cfa6daf124
makiev <makiev.grisha@gmail.com>
2025-08-27 04:11:20 +0300

    dev

--- my-nuxt-app/pages/registration.vue
+++ my-nuxt-app/pages/registration.vue
@@ -119,8 +119,7 @@ const submitForm = async () => {
 
   const tg = window.Telegram?.WebApp
   tg?.ready()
-  // const initData = tg?.initData ?? '';
-  const initData = 'query_id=AAEo8vo2AAAAACjy-jYbiqsk&user=%7B%22id%22%3A922415656%2C%22first_name%22%3A%22Gri%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22hnne2%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2FzorMNnw2jONg3vEhC0fcCsKQVn4kFJP7tPDIWfMuEb8.svg%22%7D&auth_date=1756210764&signature=3ZryenTgue1dQydXiAFIic0U68emau-mCLmBXyqyVeuHAMKxikS24xV7YjWTxhsq7HtgqeTsgCItjkB0ByTMAA&hash=ef213e5cc3a8fc05e8cf6270e3e546867d02fb7b9ef1a621b76746331ea327e3'
+  const initData = tg?.initData ?? '';
 
 
   if (!initData) {
